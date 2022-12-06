86e04149f36cce9c6cea89511718c3ca95b91ca5
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-12-06 14:31:46 -0500

    Configuration affichage appareil

--- client/src/Appareil.js
+++ client/src/Appareil.js
@@ -28,8 +28,9 @@ function Appareil(props) {
     const [cacherSenseurs, setCacherSenseurs] = useState([])
     const [descriptif, setDescriptif] = useState('')
     const [descriptifSenseurs, setDescriptifSenseurs] = useState({})
+    const [displays, setDisplays] = useState({})
     const majConfigurationHandler = useCallback(()=>{
-        const configMaj = formatterConfiguration(appareil, cacherSenseurs, descriptif, descriptifSenseurs)
+        const configMaj = formatterConfiguration(appareil, cacherSenseurs, descriptif, descriptifSenseurs, displays)
         console.debug("Maj configuration ", configMaj)
         workers.connexion.majAppareil(configMaj)
             .then(reponse=>{
@@ -38,7 +39,7 @@ function Appareil(props) {
                 setModeEdition(false)
             })
             .catch(err=>console.error("Erreur maj appareil : ", err))
-    }, [workers, dispatch, appareil, descriptif, cacherSenseurs, descriptifSenseurs, setModeEdition])
+    }, [workers, dispatch, appareil, descriptif, cacherSenseurs, descriptifSenseurs, displays, setModeEdition])
 
     useEffect(()=>{
         if(!appareil || modeEdition) return  // Aucune modification externe durant edit
@@ -47,7 +48,8 @@ function Appareil(props) {
         setDescriptif(configuration.descriptif || '')
         setCacherSenseurs(configuration.cacher_senseurs || [])
         setDescriptifSenseurs(configuration.descriptif_senseurs || {})
-    }, [modeEdition, appareil, setDescriptif, setCacherSenseurs, setDescriptifSenseurs])
+        setDisplays(configuration.displays || {})
+    }, [modeEdition, appareil, setDescriptif, setCacherSenseurs, setDescriptifSenseurs, setDisplays])
 
     const configuration = appareil.configuration || {}
     const nomAppareil = configuration.descriptif || appareil.uuid_appareil
@@ -93,6 +95,14 @@ function Appareil(props) {
                 setDescriptifSenseurs={setDescriptifSenseurs} />
 
             <p></p>
+
+            <AfficherDisplays 
+                editMode={modeEdition}
+                appareil={appareil} 
+                displays={displays} 
+                setDisplays={setDisplays} />
+
+            <p></p>
             
             {modeEdition?
                 <Row>
@@ -102,6 +112,9 @@ function Appareil(props) {
                     </Col>
                 </Row>
             :''}
+
+            <p></p>
+
         </div>
     )
 
@@ -158,7 +171,7 @@ export function AfficherSenseurs(props) {
     const configuration = appareil.configuration || {}
     const cacherSenseursNonEdit = configuration.cacher_senseurs || []
     const descriptifSenseurs = props.descriptifSenseurs || configuration.descriptif_senseurs || {}
-    
+
     const liste = useMemo(()=>{
       if(!senseurs) return
   
@@ -292,13 +305,236 @@ function AfficherValeurFormattee(props) {
     return valeur
 }
 
-function formatterConfiguration(appareil, cacherSenseurs, descriptif, descriptifSenseurs) {
+function AfficherDisplays(props) {
+    const { editMode, appareil, displays: configurationDisplays, setDisplays } = props
+
+    const majConfigurationHandler = useCallback((displayName, value)=>{
+        const displayMaj = {...configurationDisplays, [displayName]: value}
+        setDisplays(displayMaj)
+    }, [configurationDisplays, setDisplays])
+
+    const displays = useMemo(()=>{
+        let displays = appareil.displays || []
+        displays = [...displays]
+        displays.sort(sortDisplays)
+        return displays
+    }, [appareil])
+
+    return displays.map(item=>{
+
+        const { name } = item
+
+        const displayConfiguration = configurationDisplays[name] || {}
+
+        const formatDisplay = item.format
+        let Display = null
+        switch(formatDisplay) {
+            case 'text': Display = AffichageDisplayTexte; break
+            default:
+                Display = AffichageDisplayNonSupporte
+        }
+
+        return (
+            <Display key={item.name} 
+                editMode={editMode}
+                appareil={appareil} 
+                display={item} 
+                configuration={displayConfiguration}
+                majConfigurationHandler={majConfigurationHandler} />
+        )
+    })
+}
+
+function AffichageDisplayTexte(props) {
+    const { editMode, appareil, display, configuration, majConfigurationHandler } = props
+
+    const displayName = display.name
+
+    const lignes = configuration.lignes || []
+    const dureeAffichageDate = configuration.afficher_date_duree || ''
+
+    const majConfigurationDisplayHandler = useCallback(value => {
+        majConfigurationHandler(displayName, value)
+    }, [majConfigurationHandler])
+
+    const majDureeAffichageDateHandler = useCallback(event => {
+        let value = event.currentTarget.value
+        if(value) value = Number.parseInt(value)
+        else value = null
+        majConfigurationDisplayHandler({...configuration, afficher_date_duree: value})
+    }, [configuration, majConfigurationDisplayHandler])
+
+    const ajouterLigneHandler = useCallback(()=>{
+        let lignesMaj = [...lignes, {}]
+        majConfigurationDisplayHandler({...configuration, lignes: lignesMaj})
+    }, [configuration, lignes, majConfigurationDisplayHandler])
+
+    const retirerLigneHandler = useCallback(event=>{
+        const { value } = event.currentTarget
+        const ligne = Number.parseInt(value)
+        let lignesMaj = lignes.filter((_, idx)=>idx!==ligne)
+        majConfigurationDisplayHandler({...configuration, lignes: lignesMaj})
+    }, [configuration, lignes, majConfigurationDisplayHandler])
+
+    const modifierLigneHandler = useCallback(event=>{
+        let {name, value} = event.currentTarget
+        // console.debug("modifierLigneHandler %s = %s", name, value)
+        const [affichage, ligneStr, nomVar] = name.split('_')
+
+        if(['duree'].includes(nomVar)) {
+            // Convertir en int
+            value = Number.parseInt(value)
+        }
+
+        const ligne = Number.parseInt(ligneStr)
+
+        // console.debug("Modifier ligne %d = %s", ligne, value)
+        let lignesMaj = [...lignes]
+        let valeurLigne = lignesMaj[ligne] || {}
+        valeurLigne = {...valeurLigne}
+        valeurLigne[nomVar] = value
+        lignesMaj[ligne] = valeurLigne
+
+        majConfigurationDisplayHandler({...configuration, lignes: lignesMaj})
+    }, [configuration, lignes, majConfigurationDisplayHandler])
+
+    return (
+        <div>
+            <h3>Affichage {displayName}</h3>
+            <p>Format texte</p>
+            <p>Dimensions : {display.width} characteres sur {display.height} lignes</p>
+
+            <Row>
+                <Col>Duree affichage date</Col>
+                <Col>
+                    {editMode?
+                        <Form.Control type="text" value={dureeAffichageDate} onChange={majDureeAffichageDateHandler} />
+                    :
+                        <p>{dureeAffichageDate}</p>
+                    }
+                </Col>
+            </Row>
+
+            <h4>Lignes</h4>
+            {editMode?
+                <Row>
+                    <Col><Button variant="secondary" onClick={ajouterLigneHandler}>Ajouter</Button></Col>
+                </Row>
+            :''}
+
+            <Row>
+                <Col xs={12} lg={8} xl={6}>
+                    <Row>
+                        <Col xs={2} md={2}>Lignes</Col>
+                        <Col xs={10} md={8}>Masque</Col>
+                        <Col xs={0} md={2}>Duree</Col>
+                        <Col xs={0} md={0}>Senseur</Col>
+                    </Row>
+                </Col>
+            </Row>
+
+            {lignes.length===0?<p>Aucun affichage configure.</p>:''}
+
+            {lignes.map((item, idx)=>{
+
+                const masque = item.masque || ''
+                const variable = item.variable || ''
+                const duree = item.duree || 5
+
+                return (
+                    <Row key={idx}>
+                        <Col xs={12} md={8} xl={6}>
+                            <Row>
+                                <Col xs={2} md={2}>
+                                    {editMode?
+                                        <Button value={idx} variant="secondary" onClick={retirerLigneHandler}>
+                                            X
+                                        </Button>
+                                    :''}
+                                    {idx+1}
+                                </Col>
+                                <Col xs={10} md={8}>
+                                    {editMode?
+                                        <Form.Control 
+                                            type="text" 
+                                            name={[displayName, idx, 'masque'].join('_')} 
+                                            value={masque} 
+                                            placeholder="Masque d'affichage, e.g. 'Cuisine {:.1f}'"
+                                            onChange={modifierLigneHandler} />
+                                    :
+                                        <p>{masque}</p>
+                                    }
+                                </Col>
+                                <Col xs={4} md={2}>
+                                    {editMode?
+                                        <Form.Control 
+                                            type="text" 
+                                            name={[displayName, idx, 'duree'].join('_')} 
+                                            value={duree} 
+                                            onChange={modifierLigneHandler} />
+                                    :
+                                        <p>{duree}</p>
+                                    }
+                                </Col>
+                                <Col xs={0} md={2}></Col>
+                                <Col xs={8} md={10}>
+                                    {editMode?
+                                        <Form.Control 
+                                            type="text" 
+                                            name={[displayName, idx, 'variable'].join('_')} 
+                                            value={variable} 
+                                            placeholder="variable ou senseur, e.g. appareil:senseur"
+                                            onChange={modifierLigneHandler} />
+                                    :
+                                        <p>{variable}</p>
+                                    }
+                                </Col>
+                            </Row>
+                        </Col>
+                        <Col>
+                            <p>Affichage dummy...</p>
+                        </Col>
+                    </Row>
+                )
+            })}
+        </div>
+    )
+}
+
+function AffichageDisplayNonSupporte(props) {
+    const { display } = props
+    return (
+        <div>
+            <h3>Affichage {display.name}</h3>
+            <p>Cet affichage n'est pas supporte (format = {display.format})</p>
+        </div>
+    )
+}
+
+function formatterConfiguration(appareil, cacherSenseurs, descriptif, descriptifSenseurs, displays) {
     const configuration = {}
     
     if(appareil.configuration) Object.assign(configuration, appareil.configuration)
     configuration.cacher_senseurs = cacherSenseurs
     configuration.descriptif = descriptif
     configuration.descriptif_senseurs = descriptifSenseurs
+    configuration.displays = displays
 
     return {uuid_appareil: appareil.uuid_appareil, configuration}
+}
+
+
+function sortDisplays(a, b) {
+    if( a === b ) return 0
+    if(!a) return -1
+    if(!b) return 1
+
+    const nameA = a.name,
+          nameB = b.name
+
+    if(nameA === nameB) return 0
+    if(!nameA) return -1
+    if(!nameB) return 1
+
+    return nameA.localeCompare(nameB)
 }
