2b8ce31f26c0aa65fa050165ab1b5fc33d24baf7
Mathieu Dugre <mathieu.dugre@mdugre.info>
2024-08-24 17:26:51 -0400

    Fix typeValeur absent pour detail appareil

--- client/src/SenseurDetail.js
+++ client/src/SenseurDetail.js
@@ -28,11 +28,15 @@ function SenseurDetail(props) {
     const descriptifAppareil = configuration.descriptif || uuid_appareil
     const descriptif_senseurs = configuration.descriptif_senseurs || {}
     const descriptifSenseur = descriptif_senseurs[senseurId] || senseurId
-    const typeValeur = appareil.types_donnees[senseurId]
     const senseurs = appareil.senseurs || {}
     const senseurLectureCourante = senseurs[senseurId] || {}
     const timestampCourant = senseurLectureCourante.timestamp || ''
     // const typeValeur = senseurLectureCourante.type
+
+    let typeValeur = null;
+    if(appareil.types_donnees) {
+        typeValeur = appareil.types_donnees[senseurId]
+    }
 
     return (
         <div>
